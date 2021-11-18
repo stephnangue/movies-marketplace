ba79dc056aab3a937ef881aac9729fec74854d83
stephane <stephaneleonel@gmail.com>
2021-11-18 11:00:25 +0100

    Environment updated

--- src/environments/environment.sandbox.ts
+++ src/environments/environment.sandbox.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiURL: 'http://ec2-3-67-196-192.eu-central-1.compute.amazonaws.com:3000',
+  apiURL: 'http://ec2-3-121-206-200.eu-central-1.compute.amazonaws.com:3000',
 };
 
 
